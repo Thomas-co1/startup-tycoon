642dfa5ead79057dd2c34b3d520955be0f79454a
Thomas-co1 <tcollignon.74@gmail.com>
2026-05-04 09:33:41 +0200

    TP12 Partie 2: Tentative configuration SSR (WIP - passage à SSG)

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,7 +1,8 @@
+import type { ApplicationRef } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { App } from './app/app';
-import { config } from './app/app.config';
+import { config } from './app/app.config.server';
 
-const bootstrap = () => bootstrapApplication(App, config);
+const bootstrap = (): Promise<ApplicationRef> => bootstrapApplication(App, config);
 
 export default bootstrap;
